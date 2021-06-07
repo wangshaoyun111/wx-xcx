5d3e4b1a63ad1968c09a5d7130bb809551377d16
wsy <18611031643.163.com>
2021-06-07 17:44:58 +0800

    搜索页面输入框添加防抖

--- subpkg/search/search.vue
+++ subpkg/search/search.vue
@@ -10,13 +10,19 @@
   export default {
     data() {
       return {
-
+        timerId:null, // 防抖变量
+        keyVal:'' // 用户搜索关键字
       };
     },
     methods:{
       // 获取用户关键字方法
       inputHandle(e){
-        console.log(e);
+        clearTimeout(this.timerId)
+        // 防抖，获取最后一次输入内容
+        this.timerId = setTimeout(()=>{
+          this.keyVal = e
+          console.log(this.keyVal);
+        },500)
       }
     }
   }
