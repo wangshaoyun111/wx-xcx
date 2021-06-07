5ae9582b0742b177a4cff5e9bceaaaf70bf83b00
wsy <18611031643.163.com>
2021-06-07 16:54:11 +0800

    搜索组件添加props自定义背景色和圆角

--- components/my-search/my-search.vue
+++ components/my-search/my-search.vue
@@ -1,6 +1,6 @@
 <template>
-	<view class="my-search-container">
-		<view class="my-search-box">
+	<view class="my-search-container" :style="{'background-color':bgcColor}">
+		<view class="my-search-box" :style="{'border-radius':radius + 'px'}">
       <uni-icons type="search" size="17"></uni-icons>
       <text class="searchText">搜索</text>
 		</view>
@@ -14,20 +14,33 @@
 			return {
 				
 			};
-		}
+		},
+    props:{
+      // 接收传递的背景色
+      bgcColor:{
+        type:String,
+        default:'#c00000'
+      },
+      // 接收使用者传递圆角
+      radius:{
+        type:Number,
+        default:16
+      }
+    }
 	}
 </script>
 
 <style lang="scss">
 .my-search-container{
-  background-color: #c00000;
+  // 背景颜色不能不能固定，为了让不同组件都可以使用这个组件
+  // background-color: #c00000;
   height: 50px;
   padding: 0 10px;
   display: flex;
   align-items: center;
   .my-search-box{
     background-color: #FFFFFF;
-    border-radius: 16px;
+    // border-radius: 16px;
     height: 36px;
     width: 100%;
     display: flex;
