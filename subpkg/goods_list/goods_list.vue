04c50fbe8f6036441ab21d0d8ef473baee5dca6d
wsy <18611031643.163.com>
2021-06-10 14:37:45 +0800

    定义商品列表的参数

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -8,9 +8,21 @@
 	export default {
 		data() {
 			return {
-				
+				// 请求参数对象
+				queryObj: {
+				  query: '',// 查询关键词
+				  cid: '',// 商品分类Id
+				  pagenum: 1,// 页码值
+				  pagesize: 10// 每页显示多少条数据
+				},
+        total:0, // 商品列表总条数
+        goodsList:[] // 商品列表数据
 			};
-		}
+		},
+    onLoad(options) {
+      this.queryObj.query = options.query
+      this.queryObj.cid = options.cid
+    }
 	}
 </script>
 
