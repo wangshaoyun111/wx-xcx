d8a2b439dadba4e4e843a853916cf4ef3eddfff9
wsy <18611031643.163.com>
2021-06-10 17:35:50 +0800

    添加下拉刷新节流阀，防止请求未完成，继续请求

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -21,6 +21,7 @@
 				},
         total:0, // 商品列表总条数
         goodsList:[], // 商品列表数据
+        isLoading:false // 数据是否正在请求中
 			};
 		},
     onLoad(options) {
@@ -33,13 +34,17 @@
     methods:{
       // 请求商品列表的方法
       async getGoodsList(){
+        // 代表正在请求中
+        this.isLoading = true
         const{data:res} = await uni.$http.get('/api/public/v1/goods/search',this.queryObj)
         if(res.meta.status !== 200) return uni.$showTost()
+        this.isLoading = false
         this.goodsList = [...this.goodsList,...res.message.goods]
         this.total = res.message.total
       }
     },
     onReachBottom() {
+      if(this.isLoading) return
       // 页码加1
       this.queryObj.pagenum += 1
       this.getGoodsList()
