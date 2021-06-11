d2cc3770f0e1b7dad3ac04bf8d289f6231f3e244
wsy <18611031643.163.com>
2021-06-11 16:29:47 +0800

    商品详情数据渲染和结构布局

--- subpkg/goods_detail/goods_detail.vue
+++ subpkg/goods_detail/goods_detail.vue
@@ -1,6 +1,26 @@
 <template>
 	<view>
-		商品详情
+		<!-- 轮播图区域 -->
+    <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
+      <swiper-item v-for="(item,index) in goods_info.pics" :key="index">
+        <image :src="item.pics_big" ></image>
+      </swiper-item>
+    </swiper>
+    <!-- 渲染商品信息 -->
+    <view class="goods-info-container">
+      <!-- 价格 -->
+      <view class="price">￥{{goods_info.goods_price}}</view>
+      <!-- ，名称和收藏 -->
+      <view class="goods-info">
+        <view class="goods-name">{{goods_info.goods_name}}</view>
+        <view class="like">
+          <uni-icons type="star"></uni-icons>
+          <text>收藏</text>
+        </view>
+      </view>
+      <!-- 快递 -->
+      <view class="express">快递:免运费</view>
+    </view>
 	</view>
 </template>
 
@@ -28,5 +48,45 @@
 </script>
 
 <style lang="scss">
-
+  // 轮播图
+swiper{
+  height: 750rpx;
+  
+  image{
+    width: 100%;
+    height: 100%;
+  }
+}
+// 商品详情
+.goods-info-container{
+  padding: 0 10px;
+  .price{
+    color: #c00000;
+    font-size: 18px;
+    margin: 10px 0;
+  }
+  .goods-info{
+    display: flex;
+    justify-content: space-between;
+    .goods-name{
+      font-size: 13px;
+      margin-right: 10px;
+    }
+    .like{
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      border-left: 1px solid #efefef;
+      color: gray;
+      width: 120px;
+      font-size: 12px;
+    }
+  }
+  .express{
+    color: gray;
+    font-size: 12px;
+    margin: 10px 0;
+  }
+}
 </style>
