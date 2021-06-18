368086ea2637facf96bdc2a70195ef6340744a3a
wsy <18611031643.163.com>
2021-06-18 14:58:53 +0800

    购物车动态加上徽章

--- pages/cart/cart.vue
+++ pages/cart/cart.vue
@@ -5,12 +5,29 @@
 </template>
 
 <script>
+  import {mapGetters} from 'vuex'
 	export default {
 		data() {
 			return {
 				
 			};
-		}
+		},
+    onShow() {
+      // 设置徽章的方法
+      this.setBadge()
+    },
+    methods:{
+      // 动态为tabbar设置徽章方法
+      setBadge(){
+        uni.setTabBarBadge({
+          index:2,
+          text:this.total + '' // text要求时字符串类型，需要转换成字符串
+        })
+      }
+    },
+    computed:{
+      ...mapGetters('my_cart',['total'])
+    }
 	}
 </script>
 
--- unpackage/dist/dev/mp-weixin/pages/cart/cart.js
+++ unpackage/dist/dev/mp-weixin/pages/cart/cart.js
@@ -128,19 +128,36 @@ __webpack_require__.r(__webpack_exports__);
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0; //
-//
-//
-//
-//
-//
-var _default =
+/* WEBPACK VAR INJECTION */(function(uni) {Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0;
+
+
+
+
+
+
+var _vuex = __webpack_require__(/*! vuex */ 15);function ownKeys(object, enumerableOnly) {var keys = Object.keys(object);if (Object.getOwnPropertySymbols) {var symbols = Object.getOwnPropertySymbols(object);if (enumerableOnly) symbols = symbols.filter(function (sym) {return Object.getOwnPropertyDescriptor(object, sym).enumerable;});keys.push.apply(keys, symbols);}return keys;}function _objectSpread(target) {for (var i = 1; i < arguments.length; i++) {var source = arguments[i] != null ? arguments[i] : {};if (i % 2) {ownKeys(Object(source), true).forEach(function (key) {_defineProperty(target, key, source[key]);});} else if (Object.getOwnPropertyDescriptors) {Object.defineProperties(target, Object.getOwnPropertyDescriptors(source));} else {ownKeys(Object(source)).forEach(function (key) {Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key));});}}return target;}function _defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;}var _default =
 {
   data: function data() {
     return {};
 
 
-  } };exports.default = _default;
+  },
+  onShow: function onShow() {
+    // 设置徽章的方法
+    this.setBadge();
+  },
+  methods: {
+    // 动态为tabbar设置徽章方法
+    setBadge: function setBadge() {
+      uni.setTabBarBadge({
+        index: 2,
+        text: this.total + '' // text要求时字符串类型，需要转换成字符串
+      });
+    } },
+
+  computed: _objectSpread({},
+  (0, _vuex.mapGetters)('my_cart', ['total'])) };exports.default = _default;
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["default"]))
 
 /***/ })
 
